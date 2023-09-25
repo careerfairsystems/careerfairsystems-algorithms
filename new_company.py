a91f8405f99a5203721ecae4f9e1bda7acd5e07f
Stronkness <andrefrisk98@gmail.com>
2023-09-25 17:38:16 +0200

    Potential working with skeletoncode for reading data from xlsx

--- new_company.py
+++ new_company.py
@@ -1,5 +1,34 @@
 import json
 import random
+import pandas as pd
+import math
+
+# df_hosts = pd.read_excel('VÄRDAR 2023.xlsx', sheet_name='COMPANY HOST DATA', usecols='B:D')
+# df_companies = pd.read_excel('VÄRDAR 2023.xlsx', sheet_name='COMPANY HOST STATISTIK ')
+
+# companies = df_companies["Företag"].tolist()
+# host_names = df_hosts["Unnamed: 1"].tolist()
+# host_first = df_hosts["Unnamed: 2"].tolist()
+# host_remaining = df_hosts["Unnamed: 3"].tolist()
+
+# hosts = {
+#     name: {
+#         "first": first,
+#         "remaining": remaining,
+#         "advantage": 0,
+#         "assigned": []
+#     }
+#     for name, first, remaining in zip(host_names, host_first, host_remaining) if not name == "nan"
+# }
+
+# companies = {
+#     company: []
+#     for company in companies if not company == "nan"
+# }
+
+# print(companies)
+
+# exit()
 
 r1 = open('test_hosts.json')
 r2 = open('test_companies.json')
@@ -17,6 +46,7 @@ i = 1
 company_priority = sorted(companies.keys(), key=lambda c: sum(1 for h, data in hosts.items() if data["first"] == c), reverse=True)
 
 while(True):
+    print(i)
     # Check if all hosts have two companies assigned
     all_assigned = all(len(data["assigned"]) == 2 for _, data in hosts.items())
     if all_assigned:
@@ -39,56 +69,71 @@ while(True):
             
             hosts[chosen]["assigned"].append(company)
             companies[company].append(chosen)
+        
+        print(tmp_hosts)
+    
+    print("First choice done")
             
-            
-    # Second choice
-    for company in companies:
-        # If the company already has one or more hosts assigned, then it is done
-        if len(companies[company]) >= 1:
-            continue
+    # # Second choice
+    # for company in companies:
+    #     # If the company already has one or more hosts assigned, then it is done
+    #     if len(companies[company]) >= 1:
+    #         continue
 
-        tmp_hosts = []
+    #     tmp_hosts = []
 
-        for host in hosts:
-            h = hosts[host]
-            # If the host has already chosen two companies, the host is then done
-            if len(h["assigned"]) > 1:
-                continue
+    #     for host in hosts:
+    #         h = hosts[host]
+    #         # If the host has already chosen two companies, the host is then done
+    #         if len(h["assigned"]) > 1:
+    #             continue
 
-            # Check if the company is in the host's "remaining" list
-            if company in h["remaining"]:
-                tmp_hosts.append(host)
+    #         # Check if the company is in the host's "remaining" list
+    #         if company in h["remaining"]:
+    #             tmp_hosts.append(host)
 
-        if len(tmp_hosts) == i:
-            if len(tmp_hosts) > 1:
-                sorted_students = sorted(tmp_hosts, key=lambda x: (len(hosts[x]["remaining"]), -hosts[x]["advantage"]))
+    #     if len(tmp_hosts) == i:
+    #         if len(tmp_hosts) > 1:
+    #             sorted_students = sorted(tmp_hosts, key=lambda x: (len(hosts[x]["remaining"]), -hosts[x]["advantage"]))
                 
-                # The first student in the sorted list is the chosen one
-                chosen = sorted_students[0]
+    #             # The first student in the sorted list is the chosen one
+    #             chosen = sorted_students[0]
                 
-                # All other students receive an "advantage" point
-                for student in sorted_students[1:]:
-                    hosts[student]["advantage"] += 1
-            else:
-                chosen = random.choice(tmp_hosts)
+    #             # All other students receive an "advantage" point
+    #             for student in sorted_students[1:]:
+    #                 hosts[student]["advantage"] += 1
+    #         else:
+    #             chosen = random.choice(tmp_hosts)
 
-            hosts[chosen]["assigned"].append(company)
-            companies[company].append(chosen)
+    #         hosts[chosen]["assigned"].append(company)
+    #         companies[company].append(chosen)
+    
+    print("Second choice done")
 
     
     i += 1
     # break
-    if(i == 10):
+    if(i == 5):
         break
-    
+
+print("im heeeere")
 # Post-processing to ensure all hosts have two companies
 for host, data in hosts.items():
     while len(data["assigned"]) < 2:
+        assigned_company = False  # Flag to check if a company was assigned in the current iteration
+        
         for company, assigned_hosts in companies.items():
             if len(assigned_hosts) == 0:
                 data["assigned"].append(company)
                 companies[company].append(host)
+                assigned_company = True
                 break
+        
+        # If no company was assigned in the current iteration, break out of the while loop
+        if not assigned_company:
+            print("SHEEEESH")
+            break
+
 
     
 print(json.dumps(hosts, indent=2))
