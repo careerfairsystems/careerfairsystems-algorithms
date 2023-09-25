86f4b518cf551b1e7d362fff8c817966ff160cc9
Stronkness <andrefrisk98@gmail.com>
2023-09-25 16:11:59 +0200

    Almost done, again, some bugs

--- new_company.py
+++ new_company.py
@@ -13,58 +13,30 @@ companies = json.load(r2)
 # Gissningsvis är detta ett problem från "Second choice", dubbelkolla så first choice fungerar
 
 i = 1
+# Before the while loop, create a list of companies prioritized by how many students have them as their first choice
+company_priority = sorted(companies.keys(), key=lambda c: sum(1 for h, data in hosts.items() if data["first"] == c), reverse=True)
+
 while(True):
     # Check if all hosts have two companies assigned
     all_assigned = all(len(data["assigned"]) == 2 for _, data in hosts.items())
     if all_assigned:
         break
     
-    for host in hosts:
-        if len(hosts[host]) != 2:
-            break
-    
     # First choice
-    for company in companies:
-        if companies[company]: # If the company already has one host assigned, then it is done
+    for company in company_priority:
+        if companies[company]:  # If the company already has one host assigned, then it is done
             continue
-        tmp_hosts = []
-        for host in hosts:
-            h = hosts[host]
-            if len(h["assigned"]) > 1: # If the host has already chosen two companies, the host is then done
-                continue
-            if company == h["first"]:
-                tmp_hosts.append(host)
-        if len(tmp_hosts) == i:
+        tmp_hosts = [host for host, data in hosts.items() if data["first"] == company and len(data["assigned"]) < 2]
+        
+        if tmp_hosts:
             if len(tmp_hosts) > 1:
-                min_remaining_lengths = {}
-                for x in tmp_hosts:
-                    y = hosts[x]
-                    min_remaining_length = len(y["remaining"])
-                    
-                    # Update the dictionary with the minimum length
-                    min_remaining_lengths.setdefault(min_remaining_length, []).append(x)
-                    
-                # Find the person with the lowest "remaining" length
-                min_person = min(min_remaining_lengths, key=min_remaining_lengths.get)
-
-                # Get the minimum "remaining" length
-                min_length = min(min_remaining_lengths)
-
-                # Get the list of people with the minimum "remaining" length
-                min_people = min_remaining_lengths[min_length]
-                
-                # Sort students based on the number of companies they've wished for
                 sorted_students = sorted(tmp_hosts, key=lambda x: (len(hosts[x]["remaining"]), -hosts[x]["advantage"]))
-                
-                # The first student in the sorted list is the chosen one
                 chosen = sorted_students[0]
-                
-                # All other students receive an "advantage" point
                 for student in sorted_students[1:]:
                     hosts[student]["advantage"] += 1
-            else:        
-                chosen = random.choice(tmp_hosts)
-
+            else:
+                chosen = tmp_hosts[0]
+            
             hosts[chosen]["assigned"].append(company)
             companies[company].append(chosen)
             
@@ -106,7 +78,7 @@ while(True):
     
     i += 1
     # break
-    if(i == 5):
+    if(i == 10):
         break
     
 # Post-processing to ensure all hosts have two companies
